200dc4b4d164ec726a3c3dcb5adb19dfe47e76dc
Thomas <tschneider.ac@gmail.com>
2019-09-07 18:52:27 +0200

    Add Viewport Meta in header

--- src/views/general.nim
+++ src/views/general.nim
@@ -33,7 +33,7 @@ proc renderMain*(body: VNode; prefs: Prefs; title="Nitter"; titleText=""; desc="
           text titleText & " | " & title
         else:
           text title
-
+      meta(name="viewport", content="width=device-width, initial-scale=1.0")
       meta(property="og:type", content=`type`)
       meta(property="og:title", content=titleText)
       meta(property="og:description", content=desc)
