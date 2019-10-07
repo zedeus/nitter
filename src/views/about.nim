a583ad6854fbe63df5d27eca3af0cf6f79339f1c
Zed <zedeus@pm.me>
2019-10-07 17:15:56 +0200

    Display commit hash on about page
    Fixes #37

--- src/views/about.nim
+++ src/views/about.nim
@@ -1,6 +1,10 @@
 import karax/[karaxdsl, vdom]
 import markdown
 
+const
+  hash = staticExec("git log -1 --format=\"%h\"")
+  link = "https://github.com/zedeus/nitter/commit/" & hash
+
 let
   about = markdown(readFile("public/md/about.md"))
   feature = markdown(readFile("public/md/feature.md"))
@@ -8,6 +12,10 @@ let
 proc renderAbout*(): VNode =
   buildHtml(tdiv(class="overlay-panel")):
     verbatim about
+    h2: text "Instance info"
+    p:
+      text "Commit "
+      a(href=link): text hash
 
 proc renderFeature*(): VNode =
   buildHtml(tdiv(class="overlay-panel")):
