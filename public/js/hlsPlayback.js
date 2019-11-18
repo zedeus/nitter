2977972274e6255055b8f932f9a2c0a5eeaf8ecc
Zed <zedeus@pm.me>
2019-11-18 18:26:48 +0100

    Fix video playback on iOS

--- public/js/hlsPlayback.js
+++ public/js/hlsPlayback.js
@@ -1,15 +1,22 @@
 function playVideo(overlay) {
     const video = overlay.parentElement.querySelector('video');
+    const url = video.getAttribute("data-url");
     video.setAttribute("controls", "");
     overlay.style.display = "none";
 
-    const url = video.getAttribute("data-url");
-    var hls = new Hls({autoStartLoad: false});
-    hls.loadSource(url);
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-        hls.loadLevel = hls.levels.length - 1;
-        hls.startLoad();
-        video.play();
-    });
+    if (Hls.isSupported()) {
+        var hls = new Hls({autoStartLoad: false});
+        hls.loadSource(url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+            hls.loadLevel = hls.levels.length - 1;
+            hls.startLoad();
+            video.play();
+        });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = url;
+        video.addEventListened('canplay', function() {
+            video.play();
+        });
+    }
 }
