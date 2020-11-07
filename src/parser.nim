509e1dc5e8be8e9e4aa7ab709c7b7d6699a998ca
Zed <zedeus@pm.me>
2020-11-07 22:48:30 +0100

    Remove leftover from wip stuff

--- src/parser.nim
+++ src/parser.nim
@@ -114,8 +114,8 @@ proc parseVideo(js: JsonNode): Video =
     views: js{"ext", "mediaStats", "r", "ok", "viewCount"}.getStr,
     available: js{"ext_media_availability", "status"}.getStr == "available",
     title: js{"ext_alt_text"}.getStr,
-    durationMs: js{"duration_millis"}.getInt,
-    playbackType: mp4
+    durationMs: js{"duration_millis"}.getInt
+    # playbackType: mp4
   )
 
   with title, js{"additional_media_info", "title"}:
