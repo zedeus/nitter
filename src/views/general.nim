0ef90270b76c23c4d151bb1b3e219bc6c3eba7a8
Zed <zedeus@pm.me>
2022-01-03 06:31:10 +0100

    Fix video placeholder image preload

--- src/views/general.nim
+++ src/views/general.nim
@@ -91,7 +91,8 @@ proc renderHead*(prefs: Prefs; cfg: Config; titleText=""; desc=""; video="";
       link(rel="preload", type="image/png", href=bannerUrl, `as`="image")
 
     for url in images:
-      let suffix = if "400x400" in url: "" else: "?name=small"
+      let suffix = if "400x400" in url or url.endsWith("placeholder.png"): ""
+                   else: "?name=small"
       let preloadUrl = getPicUrl(url & suffix)
       link(rel="preload", type="image/png", href=preloadUrl, `as`="image")
 
