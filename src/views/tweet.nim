33dd55067b2ce5c12b1905ea06de6e1352c29cd6
Mitarashi <doggymangc@gmail.com>
2022-01-05 14:27:52 +0200

    fixed formatting and removed not needed padding

--- src/views/tweet.nim
+++ src/views/tweet.nim
@@ -181,16 +181,16 @@ proc renderCard(card: Card; prefs: Prefs; path: string): VNode =
         tdiv(class="card-content-container"):
           renderCardContent(card)
 
-func condNumber(stat: int): string {.inline.} =
+func formatStat(stat: int): string =
   if stat > 0: insertSep($stat, ',')
   else: ""
 
 proc renderStats(stats: TweetStats; views: string): VNode =
   buildHtml(tdiv(class="tweet-stats")):
-    span(class="tweet-stat"): icon "comment", condNumber(stats.replies)
-    span(class="tweet-stat"): icon "retweet", condNumber(stats.retweets)
-    span(class="tweet-stat"): icon "quote"  , condNumber(stats.quotes)
-    span(class="tweet-stat"): icon "heart"  , condNumber(stats.likes)
+    span(class="tweet-stat"): icon "comment", formatStat(stats.replies)
+    span(class="tweet-stat"): icon "retweet", formatStat(stats.retweets)
+    span(class="tweet-stat"): icon "quote", formatStat(stats.quotes)
+    span(class="tweet-stat"): icon "heart", formatStat(stats.likes)
     if views.len > 0:
       span(class="tweet-stat"): icon "play", insertSep(views, ',')
 
