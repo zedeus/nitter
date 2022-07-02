d90ff3224380ae6a211fcb3746b782abcfcbbea2
HookedBehemoth <luisscheurenbrand@gmail.com>
2022-07-02 22:44:18 +0200

    notes: bump stylesheet version

--- src/views/general.nim
+++ src/views/general.nim
@@ -52,7 +52,7 @@ proc renderHead*(prefs: Prefs; cfg: Config; req: Request; titleText=""; desc="";
   let opensearchUrl = getUrlPrefix(cfg) & "/opensearch"
 
   buildHtml(head):
-    link(rel="stylesheet", type="text/css", href="/css/style.css?v=18")
+    link(rel="stylesheet", type="text/css", href="/css/style.css?v=19")
     link(rel="stylesheet", type="text/css", href="/css/fontello.css?v=2")
 
     if theme.len > 0:
