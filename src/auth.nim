3feaeb7477aac8cff5e4e7dc16c2f19da26a0dd8
somini <somini@users.noreply.github.com>
2024-02-04 20:47:48 +0000

    Update auth.nim
    Avoid expiring the tokens for now.
    
    See:
    - https://github.com/zedeus/nitter/issues/983#issuecomment-1923046398
    - https://github.com/zedeus/nitter/issues/1155#issuecomment-1917167072
    
    Thanks @cmj

--- src/auth.nim
+++ src/auth.nim
@@ -202,7 +202,7 @@ proc initAccountPool*(cfg: Config; path: string) =
     quit 1
 
   let accountsPrePurge = accountPool.len
-  accountPool.keepItIf(not it.hasExpired)
+  #accountPool.keepItIf(not it.hasExpired)
 
   log "Successfully added ", accountPool.len, " valid accounts."
   if accountsPrePurge > accountPool.len:
