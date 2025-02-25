661be438ec0363dd3b153854765081707ab2d369
Zed <zedeus@pm.me>
2025-02-25 05:46:18 +0000

    Support both web and Android sessions

--- src/experimental/parser/session.nim
+++ src/experimental/parser/session.nim
@@ -1,15 +1,24 @@
 import std/strutils
 import jsony
 import ../types/session
-from ../../types import Session
+from ../../types import Session, SessionKind
 
 proc parseSession*(raw: string): Session =
-  let 
-    session = raw.fromJson(RawSession)
-    id = session.oauthToken[0 ..< session.oauthToken.find('-')]
+  let session = raw.fromJson(RawSession)
 
-  result = Session(
-    id: parseBiggestInt(id),
-    oauthToken: session.oauthToken,
-    oauthSecret: session.oauthTokenSecret
-  )
+  case session.kind
+  of "oauth":
+    let id = session.oauthToken[0 ..< session.oauthToken.find('-')]
+    result = Session(
+      kind: oauth,
+      id: parseBiggestInt(id),
+      oauthToken: session.oauthToken,
+      oauthSecret: session.oauthTokenSecret
+    )
+  of "cookie":
+    result = Session(
+      kind: cookie,
+      id: 999,
+      ct0: session.ct0,
+      authToken: session.authToken
+    )
--- src/experimental/types/session.nim
+++ src/experimental/types/session.nim
@@ -1,4 +1,8 @@
 type
   RawSession* = object
+    kind*: string
     oauthToken*: string
     oauthTokenSecret*: string
+    ct0*: string
+    authToken*: string
+    
