9e956150217dd6958fa8cf0f5e916bb080518642
Zed <zedeus@pm.me>
2025-11-15 07:01:03 +0100

    Remove unused skipPinned parameter

--- src/routes/timeline.nim
+++ src/routes/timeline.nim
@@ -27,8 +27,7 @@ template skipIf[T](cond: bool; default; body: Future[T]): Future[T] =
   else:
     body
 
-proc fetchProfile*(after: string; query: Query; skipRail=false;
-                   skipPinned=false): Future[Profile] {.async.} =
+proc fetchProfile*(after: string; query: Query; skipRail=false): Future[Profile] {.async.} =
   let
     name = query.fromUser[0]
     userId = await getUserId(name)
@@ -71,7 +70,7 @@ proc showTimeline*(request: Request; query: Query; cfg: Config; prefs: Prefs;
       html = renderTweetSearch(timeline, prefs, getPath())
     return renderMain(html, request, cfg, prefs, "Multi", rss=rss)
 
-  var profile = await fetchProfile(after, query, skipPinned=prefs.hidePins)
+  var profile = await fetchProfile(after, query)
   template u: untyped = profile.user
 
   if u.suspended:
